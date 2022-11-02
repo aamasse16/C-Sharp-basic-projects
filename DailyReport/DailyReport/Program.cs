586ab5fdd5016ac241c6e0c95005e915eb3d8301
aamasse16 <112958171+aamasse16@users.noreply.github.com>
2022-11-02 13:37:31 -0400

    made another change to daily report
    changed the string to an int and actually saved it this time

--- DailyReport/DailyReport/Program.cs
+++ DailyReport/DailyReport/Program.cs
@@ -19,7 +19,7 @@ namespace DailyReport
             string course = Console.ReadLine();
             //ask for the current page number and stores it in "pageNum"
             Console.WriteLine("What is the page number you are on?");
-            string pageNum = Console.ReadLine();
+            int pageNum = Convert.ToInt32(Console.ReadLine());
             //ask if any help is needed and stores it as string in "needHelp" then converts it to bool and is stored in "help"
             Console.WriteLine("Is there anything you need help with? (please answer \"true\" or \"false\")");
             string needHelp = Console.ReadLine();
@@ -32,7 +32,7 @@ namespace DailyReport
             string feedback = Console.ReadLine();
             //ask for the number of hours studied and stores it in "hours studied"
             Console.WriteLine("How many hours did you study today?");
-            string hoursStudied = Console.ReadLine();
+            int hoursStudied = Convert.ToInt32(Console.ReadLine());
             //says thank you for filling ou the form and says goodbye
             Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
 
