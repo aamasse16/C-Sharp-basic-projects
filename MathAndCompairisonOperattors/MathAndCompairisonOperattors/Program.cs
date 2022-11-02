56de633ded5fcec86c993ded59f40dfefb420cf5
aamasse16 <112958171+aamasse16@users.noreply.github.com>
2022-11-02 13:17:57 -0400

    made changes to daily report
    changed the car type on page num and hours studied to int

---
+++ MathAndCompairisonOperattors/MathAndCompairisonOperattors/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathAndCompairisonOperattors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int total = 5 + 10;
+            //int otherTotal = 12 + 22;
+            //int combined = total + otherTotal;
+            //Console.WriteLine(combined);
+            //Console.Read();
+
+            //int difference = 10 - 5;
+            //Console.WriteLine("Ten minus Five = " + difference.ToString());
+            //Console.Read();
+
+            //int product = 10 * 5;
+            //Console.WriteLine(product);
+            //Console.Read();
+
+            //double quotient = 100.0 / 17.0;
+            //Console.WriteLine(quotient);
+            //Console.Read();
+
+            int remainder = 11 % 2;
+            Console.WriteLine(remainder);
+            Console.Read();
+
+        }
+    }
+}
---
+++ mathPractice/mathPractice/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mathPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 6;
+            int num2 = 4;
+
+            int add = num1 + num2;
+            Console.WriteLine(add);
+
+            int subtract = num1 - num2;
+            Console.WriteLine(subtract);
+
+            int multiply = num1 * num2;
+            Console.WriteLine(multiply);
+
+            int divide = num1 / num2;
+            Console.WriteLine(divide);
+
+            string str1 = "hello, ";
+
+            string str2 = "world!";
+
+            string full = str1 + str2;
+            Console.WriteLine(full);
+
+            Console.Read();
+        }
+    }
+}
