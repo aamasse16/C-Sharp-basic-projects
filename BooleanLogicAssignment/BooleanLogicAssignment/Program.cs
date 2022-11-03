8fb95c9f25d48f756a824673295cd00447ee2c92
aamasse16 <112958171+aamasse16@users.noreply.github.com>
2022-11-03 10:23:37 -0400

    added Boolean logic assignement
    added assignment that checks if user is over 15 has ever had a dui or has more than 3 speeding tickets

---
+++ BooleanLogicAssignment/BooleanLogicAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogicAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How old are you?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI? (please type 'true' or 'false')");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int speedingTickets = Convert.ToInt32(Console.ReadLine());
+
+            bool qualified = age > 15 && dui == false && speedingTickets <= 3;
+
+            Console.WriteLine("Qualified: " + qualified);
+            Console.Read();
+        }
+    }
+}
--- MathAndCompairisonOperatorAssignment/MathAndCompairisonOperatorAssignment/Program.cs
+++ MathAndCompairisonOperatorAssignment/MathAndCompairisonOperatorAssignment/Program.cs
@@ -9,26 +9,26 @@ namespace MathAndCompairisonOperatorAssignment
     class Program
     {
         static void Main(string[] args)
-        {
+        {   //welcoming to the program
             Console.WriteLine("Anonymous Income Comparison Program");
-            decimal personOneHourlyRate = 15.00m;
+            decimal personOneHourlyRate = 15.00m; //declairing variables
             decimal personOneHoursWorked = 36.00m;
-            Console.WriteLine("Person 1 \nHourly Rate: " + personOneHourlyRate + "\nHours Worked per week: " + personOneHoursWorked);
+            Console.WriteLine("Person 1 \nHourly Rate: " + personOneHourlyRate + "\nHours Worked per week: " + personOneHoursWorked);//using variables with string concatenation
 
-            decimal personTwoHourlyRate = 20.00m;
+            decimal personTwoHourlyRate = 20.00m;//declaring variables
             decimal personTwoHoursWorked = 25.00m;
-            Console.WriteLine("Person 2 \nHourly Rate: " + personTwoHourlyRate + "\nHours Worked per week: " + personTwoHoursWorked);
+            Console.WriteLine("Person 2 \nHourly Rate: " + personTwoHourlyRate + "\nHours Worked per week: " + personTwoHoursWorked);//using variables with string concatenation
 
-            decimal personOneSalary = personOneHourlyRate * personOneHoursWorked * 52;
+            decimal personOneSalary = personOneHourlyRate * personOneHoursWorked * 52; //using the above variables to calculate each persons salary
 
             decimal personTwoSalary = personTwoHourlyRate * personTwoHoursWorked * 52;
 
-            Console.WriteLine("Annual salary of Person 1: " + personOneSalary);
+            Console.WriteLine("Annual salary of Person 1: " + personOneSalary);//using string concatenation to display the salarys with some text
             Console.WriteLine("Annual salary of Person 2: " + personTwoSalary);
 
-            bool whoMakesMore = personTwoSalary > personOneSalary;
+            bool whoMakesMore = personTwoSalary > personOneSalary;//using boolean logic to compair the 2 slaries
 
-            Console.WriteLine("It is " + whoMakesMore + " that person two makes more than person one in a year.");
+            Console.WriteLine("It is " + whoMakesMore + " that person two makes more than person one in a year."); //using string concatenation and the boolean varible to tell who makes more in a year
 
             Console.Read();
         }
