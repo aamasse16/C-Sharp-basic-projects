5693a0e643b27ae8541baceef512bb8bb0ad8d6d
aamasse16 <112958171+aamasse16@users.noreply.github.com>
2022-11-08 12:28:09 -0500

    added projects
    added exception handling, and a new console app assignment

---
+++ ConsoleAppStringAndInt/ConsoleAppStringAndInt/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppStringAndInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> intList = new List<int>() {4, 6, 8, 10};//initializing a list of ints
+
+            try//try block of code
+            {
+                Console.WriteLine("Please give me a number.");//asking for user input
+                int number = Convert.ToInt32(Console.ReadLine());//assigning user input to "number"
+
+                foreach (int num in intList)//each element in the list will have the following code executed on it
+                {
+                    int dividedNumber;//new int to be used for each element
+                    dividedNumber = num / number;//new int will be set = to num / number
+                    Console.WriteLine(dividedNumber);//print each dividedNumber to the console
+                }
+            }
+            catch (FormatException ex)//exception if an int isnt passed from the user
+            {
+                Console.WriteLine("Please input an integer");
+            }
+            catch (DivideByZeroException ex)//exception if the user inputs a 0
+            {
+                Console.WriteLine("Please dont input a 0");
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        try
+        {
+            Console.WriteLine("Pick a number.");
+            int numOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pick a second number.");
+            int numTwo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Dividing the two.....");
+            int numThree = numOne / numTwo;
+            Console.WriteLine(numOne + " divided by " + numTwo + " equals " + numThree);
+            Console.Read();
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("Please type a whole number.");
+            return;
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("Plese dont divide by 0.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Console.Read();
+        }
+        Console.Read();
+        }
+    }
