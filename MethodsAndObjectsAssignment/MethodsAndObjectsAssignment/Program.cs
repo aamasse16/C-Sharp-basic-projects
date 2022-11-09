dcce1e7899447b6613c436ae963d2ffac4c0231f
aamasse16 <112958171+aamasse16@users.noreply.github.com>
2022-11-09 12:29:42 -0500

    now i actually added the assignment
    added methodsandobjectsassignment

---
+++ MethodsAndObjectsAssignment/MethodsAndObjectsAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjectsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee number1 = new Employee();
+            number1.FirstName = "Sample";
+            number1.LastName = "Student";
+
+            number1.SayName();
+            Console.Read();
+            
+        }
+    }
+}
