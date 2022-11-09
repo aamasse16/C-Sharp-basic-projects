2f2f77f1eee56c0ce7cdf44088d894cc656e2156
aamasse16 <112958171+aamasse16@users.noreply.github.com>
2022-11-08 20:40:18 -0500

    added MainMethodAssignment
    added project to practice methods and classes

---
+++ MainMethodAssignment/MainMethodAssignment/MathOperation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class MathOperation
+    {
+        public int addNum(int num1)
+        {
+            int result = num1 + 5;
+            return result;
+        }
+
+        public int addNum(decimal dec1)
+        {
+            decimal result2 = dec1 / 2;
+            return Convert.ToInt32(result2);
+        }
+
+        public string addNum(string str1)
+        {
+            int int1 = Convert.ToInt32(str1);
+            int result3 = int1 * 2;
+            string convertedString = result3.ToString();
+            return convertedString;
+        }
+    }
+}
---
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please give me a number to perform an operation on.");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            MathOperation add = new MathOperation();//creating an instance of the mathOperation class
+
+            int number = add.addNum(userNum);//using my new class and the method in the class to get a new number
+
+            Console.WriteLine("Your number plus five = " + number);//printing the output to the console with some text
+
+            Console.WriteLine("Please give me a decimal to perform an operation on.");
+            decimal userDec = Convert.ToDecimal(Console.ReadLine());
+
+            int number2 = add.addNum(userDec);//using the same class instance and the same method name to do my second operation
+
+            Console.WriteLine("Your decimal divided by two and rounded down = " + number2);//printing the output to the console with text
+
+            Console.WriteLine("Please give me one more number to multiply");//ask user for one last number
+            string userString = Console.ReadLine();//get number from user and assign it to userString
+
+            string multipliedString = add.addNum(userString);
+
+            Console.WriteLine("Your number times 2 = " + multipliedString);//retun
+
+            Console.Read();
+
+
+        }
+    }
+}
