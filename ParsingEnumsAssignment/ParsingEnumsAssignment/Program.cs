a7fe6c95f69e5460464000a4238dc69ac95cc774
aamasse16 <112958171+aamasse16@users.noreply.github.com>
2022-11-10 14:16:38 -0500

    added new project
    added StructAssingment where i made a struct and added properties to it and printed it

--- ParsingEnumsAssignment/ParsingEnumsAssignment/Program.cs
+++ ParsingEnumsAssignment/ParsingEnumsAssignment/Program.cs
@@ -13,14 +13,14 @@ namespace ParsingEnumsAssignment
             Console.WriteLine("Please enter your favorite day of the week.");//asking user for input
 
             string userDay = Console.ReadLine();//assigning userDay to user input
-            try
+            try//try this 
             {
                 Day favDay = (Day)Enum.Parse(typeof(Day), userDay.ToLower());//assigning the variable userday to the enum and checking to see if it matches 
                                                                              //one of the enum values 
                 Console.WriteLine(favDay);//finally printing the enum value if it matched in the above code if not an error will be thrown
                 Console.Read();
             }
-            catch 
+            catch//if the try fails print this
             {
                 Console.WriteLine("Please enter an actual day of the week.");
                 Console.Read();
---
+++ SrtuctAssignment/SrtuctAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SrtuctAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number newNumber = new Number
+            {
+                Amount = Convert.ToDecimal(1.4343)
+            };
+
+            Console.WriteLine(newNumber.Amount);
+            Console.ReadLine();
+        }
+        public struct Number
+        {
+            public decimal Amount { get; set; }
+        }
+    }
+}
