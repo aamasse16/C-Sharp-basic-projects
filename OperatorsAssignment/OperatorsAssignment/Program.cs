b8618662f138494f726fbf57c4417b5b46e16fb4
aamasse16 <112958171+aamasse16@users.noreply.github.com>
2022-11-10 13:14:50 -0500

    fixed project
    added code to use the overloaded operator == in OperatorsAssignment

--- OperatorsAssignment/OperatorsAssignment/Program.cs
+++ OperatorsAssignment/OperatorsAssignment/Program.cs
@@ -10,6 +10,21 @@ namespace OperatorsAssignment
     {
         static void Main(string[] args)
         {
+            Employee emp1 = new Employee();
+            emp1.firtName = "Aaron";
+            emp1.lastName = "Masse";
+            emp1.Id = 1;
+
+            Employee emp2 = new Employee();
+            emp2.firtName = "Aaron";
+            emp2.lastName = "Masse";
+            emp2.Id = 1;
+
+            if (emp1 == emp2)
+            {
+                Console.WriteLine("Employee 1 and Employee 2 are the same.");
+            }
+            Console.Read();
         }
     }
 }
