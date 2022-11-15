92e4299487e8034ab5ccaa95b5d0619ef31f95aa
aamasse16 <112958171+aamasse16@users.noreply.github.com>
2022-11-15 14:50:19 -0500

    made changes to TwentyOne
    pushing to github for instructor to look at

--- TryCatchAssignment/TryCatchAssignment/Program.cs
+++ TryCatchAssignment/TryCatchAssignment/Program.cs
@@ -12,21 +12,21 @@ namespace TryCatchAssignment
         {
             try
             {
-                Console.WriteLine("Please input your age.");
-                int userAge = Convert.ToInt32(Console.ReadLine());
-                if (userAge < 0)
+                Console.WriteLine("Please input your age.");//ask user for age
+                int userAge = Convert.ToInt32(Console.ReadLine());//assign user input to var userAge
+                if (userAge < 0)//if the user inputs a number below 0 the following exception will be thrown
                 {
                     throw new FormatException();
                 }
-                Console.WriteLine("So you are " + userAge + " years old?");
+                Console.WriteLine("So you are " + userAge + " years old?");//if the above if statment isnt hit then this code will be run
             }
-            catch (FormatException)
+            catch (FormatException)//catch for format exceptions
             {
                 Console.WriteLine("Please input a positive number for your age.");
                 Console.ReadLine();
                 return;
             }
-            catch (Exception)
+            catch (Exception)//catch for generic exceptions
             {
                 Console.WriteLine("An error occurred, please try again.");
                 Console.ReadLine();
--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using System.IO;
 using Casino;
 using Casino.TwentyOne;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace TwentyOne
 {
@@ -44,15 +46,17 @@ namespace TwentyOne
                     {
                         game.Play();
                     }
-                    catch (FraudException)
+                    catch (FraudException ex)
                     {
                         Console.WriteLine("Security! Kick this person out.");
+                        UpdateDbWithException(ex);
                         Console.Read();
                         return;
                     }
-                    catch (Exception)
+                    catch (Exception ex)
                     {
                         Console.WriteLine("An error occurred. Please contact your System Admin.");
+                        UpdateDbWithException(ex);
                         Console.ReadLine();
                         return;
                     }
@@ -63,6 +67,31 @@ namespace TwentyOne
             }
             Console.WriteLine("Feel free to look around the casino. Bye for now.");
             Console.Read();
+        }
+
+        private static void UpdateDbWithException(Exception ex)
+        {
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=TwentyOneGame;Integrated Security=True;Connect Timeout=30;
+                                        Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            string queryString = @"INSERT INTO Exceptions (ExceptionType, ExceptionMessage, TimeStamp) VALUES
+                                   (@ExceptionType, @ExceptionMessage, @TimeStamp)";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Parameters.Add("@ExceptionType", SqlDbType.VarChar);
+                command.Parameters.Add("@ExceptionMessage", SqlDbType.VarChar);
+                command.Parameters.Add("@TimeStamp", SqlDbType.DateTime);
+
+                command.Parameters["@ExceptionType"].Value = ex.GetType().ToString();
+                command.Parameters["@ExceptionMessage"].Value = ex.Message;
+                command.Parameters["@TimeStamp"].Value = DateTime.Now;
+
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
         }
     }
 }
