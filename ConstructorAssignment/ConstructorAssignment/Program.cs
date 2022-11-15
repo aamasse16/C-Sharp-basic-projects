56c6b8c0253d434b05cd1c436e2a12ddc4a09f73
aamasse16 <112958171+aamasse16@users.noreply.github.com>
2022-11-15 11:21:49 -0500

    added new project
    added constructor assignment

---
+++ ConstructorAssignment/ConstructorAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ConstructorAssignment.Person;
+
+namespace ConstructorAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string name = "Aaron";
+
+            var str = "apples";
+
+            Student Luis = new Student("Luis");
+            Console.WriteLine(Luis.firstName + " is the newest student here.");
+            Console.Read();
+        }
+    }
+}
