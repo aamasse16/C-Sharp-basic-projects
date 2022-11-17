b4b8a6cbc10e1c96c3d171dfc30c10c4c5dfa835
aamasse16 <112958171+aamasse16@users.noreply.github.com>
2022-11-16 21:54:03 -0500

    added code
    added to constructorassignment

--- ConstructorAssignment/ConstructorAssignment/Program.cs
+++ ConstructorAssignment/ConstructorAssignment/Program.cs
@@ -10,6 +10,47 @@ namespace ConstructorAssignment
     {
         static void Main(string[] args)
         {
+            const string str = "string";//create a const variable
+
+            var num1 = 1;//create a var using the keyword "var"
+
+        }
+    }
+    class Pizza
+    {
+        string bread;
+        string sauce;
+        string cheese;
+        string topping;
+        
+        public Pizza()//here is an example of constructor overloading
+        {
+        }
+
+        public Pizza(string bread)
+        {
+            this.bread = bread;
+        }
+
+        public Pizza(string bread, string sauce)
+        {
+            this.bread = bread;
+            this.sauce = sauce;
+        }
+
+        public Pizza(string bread, string sauce, string cheese): this(cheese, "mozzarella")//and this line is constructor chaining
+        {
+            this.bread = bread;
+            this.sauce = sauce;
+            this.cheese = cheese;
+        }
+
+        public Pizza(string bread, string sauce, string cheese, string topping)
+        {
+            this.bread = bread;
+            this.sauce = sauce;
+            this.cheese = cheese;
+            this.topping = topping;
         }
     }
 }
