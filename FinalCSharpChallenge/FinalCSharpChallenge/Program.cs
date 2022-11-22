a67bb174907aac0c48567816309982c7c9a13879
aamasse16 <112958171+aamasse16@users.noreply.github.com>
2022-11-21 19:10:49 -0500

    added new project
    added FinalCSharpChallenge where i used code first to make a database that can be added to from my code

---
+++ FinalCSharpChallenge/FinalCSharpChallenge/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalCSharpChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Aaron" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
